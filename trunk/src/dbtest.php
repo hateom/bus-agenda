8731c34fb8aef5e0efd4881ffcaefdf4c12e1e1b
tomasz.huczek <tomasz.huczek@f655eb90-c23e-0410-93b9-7166266ad68b>
2008-01-17 10:35:48 +0000

    encoding

--- trunk/src/dbtest.php
+++ trunk/src/dbtest.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="pl" xml:lang="pl">
+        <head profile="http://gmpg.org/xfn/11">
+                <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8"/>
+                <title>Bus Agenda ::Tomasz Huczek &amp; Andrzej Jasiński :: &copy;</title>
+                <meta name="description" content="content" />
+                <meta name="keywords" content="Bus Agenda" />
+                <link rel="stylesheet" href="./style/main.css" type="text/css" media="screen" />
+        </head>
+<body>
+
 <?php
 
     include_once('dbdriver.php');
@@ -47,4 +58,5 @@
     $db->release();
 
 ?>
-
+</body>
+</html>
